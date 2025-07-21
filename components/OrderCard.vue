cf744a9755090548abb8bb60a125ae216f9becd6
Thas94 <thasmalematja@gmail.com>
2025-07-21 15:52:05 +0200

    Replaced props with fallthrough attributes for OrderCard component

--- components/OrderCard.vue
+++ components/OrderCard.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="flex items-center justify-between mb-2">
-        <h2 class="text-lg font-semibold">Order #{{ props.order.orderId }}</h2>
-        <span :class="props.order.orderStatus === 'Order Placed' ? 'text-yellow-400' :
-            props.order.orderStatus === 'Payment Processing' ? 'text-yellow-600' :
-                props.order.orderStatus === 'Order Received' ? 'text-yellow-800' :
-                    props.order.orderStatus === 'Picking and Packing' ? 'text-yellow-950' :
-                        props.order.orderStatus === 'Shipping' ? 'text-green-200' :
-                            props.order.orderStatus === 'Order Shipped' ? 'text-green-400' :
+        <h2 class="text-lg font-semibold">Order #{{ order.orderId }}</h2>
+        <span :class="order.orderStatus === 'Order Placed' ? 'text-yellow-400' :
+            order.orderStatus === 'Payment Processing' ? 'text-yellow-600' :
+                order.orderStatus === 'Order Received' ? 'text-yellow-800' :
+                    order.orderStatus === 'Picking and Packing' ? 'text-yellow-950' :
+                        order.orderStatus === 'Shipping' ? 'text-green-200' :
+                            order.orderStatus === 'Order Shipped' ? 'text-green-400' :
                                 'text-green-600'" class="font-medium cursor-pointer" @click="isVisible = true">
-            {{ props.order.orderStatus }}
+            {{ order.orderStatus }}
         </span>
     </div>
-    <p class="text-sm text-gray-600">Placed on: {{ props.order.createdDate }}</p>
+    <p class="text-sm text-gray-600">Placed on: {{ order.createdDate }}</p>
     <ul class="mt-2 text-sm text-gray-700 list-disc list-inside">
-        <li v-for="item in props.order.orderDetailsObj">{{ item.name }} - {{ item.price }}</li>
+        <li v-for="item in order.orderDetailsObj">{{ item.name }} - {{ item.price }}</li>
     </ul>
     <Dialog v-model:visible="isVisible" modal header='Order Tracking' :style="{ width: '25rem', height: '30rem' }">
-        <span class="block mb-8 text-surface-500">Order #{{ props.order.orderId }}</span>
+        <span class="block mb-8 text-surface-500">Order #{{ order.orderId }}</span>
         <div class="flex gap-2">
             <ul class="space-y-4">
                 <li v-for="(step, index) in trackingStatuses" :key="index" class="flex items-center">
@@ -31,9 +31,12 @@
 
 <script setup lang="ts">
 import { type OrderDetails } from '~/models/orderDetails';
-const props = defineProps<{ order: OrderDetails }>()
+// const props = defineProps<{ order: OrderDetails }>()
 const statusIndex = ref(0)
 const isVisible = ref(false);
+const {orders} = useOrderStore()
+const attrs = useAttrs()
+const order = computed(() => orders[Number(attrs['data-index'])])
 
 const trackingStatuses = ref([
     { label: 'Order Placed', completed: false },
@@ -46,7 +49,7 @@ const trackingStatuses = ref([
 ])
 
 onMounted(() => {
-    statusIndex.value = trackingStatuses.value.findIndex(x => x.label === props.order.orderStatus)
+    statusIndex.value = trackingStatuses.value.findIndex(x => x.label === order.value.orderStatus)
     for(var i = 0;i < trackingStatuses.value.length;i++){
         if(i <= statusIndex.value)
             trackingStatuses.value[i].completed = true
--- pages/Orders/index.vue
+++ pages/Orders/index.vue
@@ -4,8 +4,8 @@
     <div class="space-y-4">
       <Loader v-if="loading" />
       <div v-else-if="orders.length > 0">
-        <div v-for="order in orders" :key="order.orderId" class="p-4 bg-white border border-gray-200 rounded shadow">
-          <OrderCard :order="order" />
+        <div v-for="(order, index) in orders" :key="order.orderId" class="p-4 bg-white border border-gray-200 rounded shadow">
+          <OrderCard :data-index="index" />
         </div>
       </div>
       <p v-else class="text-xl">Oops, no orders found.</p>
