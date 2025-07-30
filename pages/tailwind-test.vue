ca9f4fb2a6107de0890492df99f48c63eb417096
Thas94 <thasmalematja@gmail.com>
2025-07-30 13:51:45 +0200

    refactored

--- pages/tailwind-test.vue
+++ pages/tailwind-test.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="flex items-center gap-2">
-        <h1 class="text-blue-500 ">tailwind</h1>
-        <h1 class="text-red-500">tailwind</h1>
+    <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+        <div>
+            <img class="size-48 shadow-xl rounded-md" alt="" src="/public/cover.png" />
+        </div>
+        <div class="flex items-center md:items-start">
+            <span class="text-2xl font-medium">Class Warfare</span>
+            <span class="font-medium text-sky-500">The Anti-Patterns</span>
+            <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+                <span>No. 4</span>
+                <span>·</span>
+                <span>2025</span>
+            </span>
+        </div>
     </div>
 </template>
 
@@ -9,6 +19,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
